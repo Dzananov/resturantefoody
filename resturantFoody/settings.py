8f5e99d3a644b47f4e4317e5f04173901b993076
Dzananov <danni_daa@hotmail.com>
2022-10-02 19:22:08 +0000

    correct spellingerror in settings.py, ALLOWED_HOSTS 'resturantefoody'

--- resturantFoody/settings.py
+++ resturantFoody/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['resturantefoody.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['resturantfoody.herokuapp.com', 'localhost']
 
 
 # Application definition
