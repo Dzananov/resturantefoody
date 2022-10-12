d706b3ed36f2eb9b96dd1dd3d37d5a9109846cce
Dzananov <danni_daa@hotmail.com>
2022-10-12 18:41:10 +0000

    change function in views book_a_table, testing

--- bookAtable/tests.py
+++ bookAtable/tests.py
@@ -3,3 +3,11 @@ from django.test import TestCase
 # Create your tests here.
 def get_avalible_bookings():
     return {(guests, time) for time in dict(TIME_FRAME).keys() for day in dict(GUESTS).keys()}
+class book_a_Table(generic.ListView):
+"""This shows the bookingform if user is loged in. """
+    model = Reservation
+    template_name = "booking_form.html"
+    if request.method == 'POST':
+        form_class = BookingForm(data=request.POST)
+        if form.is_valid():
+            BookingForm= save
