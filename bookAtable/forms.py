f5b162c5735ffda00d91f9e15c8923e8bfde996c
Dzananov <danni_daa@hotmail.com>
2022-10-13 18:19:33 +0000

    bugg in views and forms, have to fix. Template telling unvalid choices on guest and tim

--- bookAtable/forms.py
+++ bookAtable/forms.py
@@ -1,10 +1,25 @@
 from django import forms
-from .models import Reservation
 
 
-class BookingForm(forms.ModelForm):
-    class Meta:
-        model = Reservation
-        fields = (
-            'name', 'email', 'guests', 'time',
-        )
+GUESTS = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+)
+TIME_FRAME = (
+    ('16-17', '16-17'),
+    ('17-18', '17-18'),
+    ('18-19', '18-19'),
+    ('19-20', '19-20'),
+)
+
+
+class BookingForm(forms.Form):
+    name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    guests = forms.ChoiceField(choices=GUESTS, required=True)
+    time = forms.ChoiceField(choices=TIME_FRAME, required=True)
+    date = forms.DateField()
